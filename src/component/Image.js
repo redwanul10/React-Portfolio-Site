9810f06fb81208d03fc4481d4b1a48af4be097ec
unknown <redwanulislam12@gmail.com>
2020-09-30 16:55:43 +0600

    fixed image issues

--- src/component/Image.js
+++ src/component/Image.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { SRLWrapper } from "simple-react-lightbox";
+// import { SRLWrapper } from "simple-react-lightbox";
 import ScrollAnimation from 'react-animate-on-scroll';
 
 
-const Image = ({url,delay,anim}) => {
+const Image = ({ url, delay, anim }) => {
 
-    const options={
-        enablePanzoom:false,
-        showThumbnails:false,
-        thumbnailsOpacity:0
+    const options = {
+        enablePanzoom: false,
+        showThumbnails: false,
+        thumbnailsOpacity: 0
     }
 
     const iniVisible = anim ? false : true;
 
     return (
         <div className="col-md-4">
-            <SRLWrapper {...options}>
-                <ScrollAnimation delay={delay}animateIn={anim} animateOnce={true} initiallyVisible={iniVisible}>
+            {/* <SRLWrapper {...options}> */}
+            <ScrollAnimation delay={delay} animateIn={anim} animateOnce={true} initiallyVisible={iniVisible}>
                 <div className="imageWrapper">
-                    <img src={url} alt="..."/>
+                    <img src={url} alt="..." />
                 </div>
-                </ScrollAnimation>
-            </SRLWrapper>
+            </ScrollAnimation>
+            {/* </SRLWrapper> */}
         </div>
     );
 };
--- src/component/SingleService.js
+++ src/component/SingleService.js
@@ -3,15 +3,17 @@ import Zoom from 'react-reveal/Zoom';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 
-const SingleService = ({service,delay}) => {
+const SingleService = ({ service, delay }) => {
     console.log(service.animation)
-    const iniVisible = service.animation ? false:true
+    const iniVisible = service.animation ? false : true
 
     return (
         <div className="col-md-3">
             <ScrollAnimation animateIn={service.animation} delay={delay} animateOnce={true} initiallyVisible={iniVisible}>
                 <div class="services-block">
-                    <img src={service.photo.mediaItemUrl} alt=""/>
+                    {/* <img src={service.photo.mediaItemUrl} alt=""/> */}
+                    <i class={service.photo.mediaItemUrl} aria-hidden="true"></i>
+
                     <span>{service.name}</span>
                     <p class="separator">{service.description}</p>
                 </div>
